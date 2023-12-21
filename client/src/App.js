60b7579005fbf8570ce12f02c274ddb4a4924485
Umair <umairmunawar536@gmail.com>
2023-12-21 11:26:14 +0500

    communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [receivedMessages, setReceivedMessages] = useState([]);
+  const [room, setRoom] = useState()
+  const [joinStatus, setJoinStatus] = useState()
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room)
+
+    }
+  }
+
+  const sendMessage = () => {
+    socket.emit("send_message", {
+      message: inputValue,
+      room
+    });
+    setInputValue("");
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setReceivedMessages(prevMessages => [...prevMessages, data]);
+    });
+
+    socket.on("message_history", (data) => {
+      setReceivedMessages(data);
+    });
+    socket.on("status", (data) => {
+      setJoinStatus(data);
+    });
+
+    return () => {
+      socket.off("receive_message");
+      socket.off("status");
+    };
+  }, []);
+
+  console.log(joinStatus)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="number"
+        placeholder="room id"
+        value={room}
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <button onClick={joinRoom}>join room</button>
+      <input
+        type="text"
+        placeholder="message..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+
+        <b> {joinStatus}</b> has joined the room
+        <p>Received Messages:</p>
+        {receivedMessages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -15,6 +15,22 @@ const io = new Server(server, {
     }
 })
 
+io.on("connection", (socket) => {
+
+    socket.on("join_room", (data) => {
+        if (data) {
+            socket.emit("status", socket.id)
+        }
+        socket.join(data)
+    });
+
+    socket.on("send_message", (data) => {
+
+        socket.to(data.room).emit("receive_message", data.message)
+    });
+
+})
+
 server.listen(3001, () => {
     console.log("Server is running!!")
 })
